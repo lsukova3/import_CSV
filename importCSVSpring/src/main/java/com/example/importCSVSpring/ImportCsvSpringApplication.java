36dc0f4150b0d3567e3eeacd7b5b2efa2ea90114
lsukova3 <127726806+lsukova3@users.noreply.github.com>
2023-05-30 21:01:00 +0200

    Add files via upload

---
+++ importCSVSpring/src/main/java/com/example/importCSVSpring/ImportCsvSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.importCSVSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportCsvSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportCsvSpringApplication.class, args);
+	}
+
+}
---
+++ importCSVSpring/src/test/java/com/example/importCSVSpring/ImportCsvSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.importCSVSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportCsvSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
